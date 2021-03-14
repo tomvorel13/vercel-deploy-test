ed78ae9109d39b1dacb75ebb8194a36e55672a95
Tom Vorel <tomvorel13@gmail.com>
2021-03-14 17:08:01 +0100

    New branch

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Deploy to Vercel test</h1>
+      <h1>Deploy to Vercel test 2</h1>
     </div>
   )
 }
